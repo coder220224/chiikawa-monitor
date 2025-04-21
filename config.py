26c9323fd358327cdc4c2eb48798d286abb77fa4
摳得柴柴 <87404598+coder220224@users.noreply.github.com>
2025-04-21 16:02:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
-WORK_DIR = os.path.dirname(os.path.abspath(__file__))
+# Render 的持久化儲存路徑
+WORK_DIR = '/opt/render/project/data'
+
+# 如果目錄不存在，建立它
+if not os.path.exists(WORK_DIR):
+    os.makedirs(WORK_DIR)
