cf14a0f0040c2cf93278bb37929a0e4de6802a40
摳得柴柴 <87404598+coder220224@users.noreply.github.com>
2025-05-29 19:12:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 MONGODB_URI = os.getenv('MONGODB_URI')
 WORK_DIR = os.path.dirname(os.path.abspath(__file__))
+DISCORD_WEBHOOK_URL = "https://discordapp.com/api/webhooks/1376888371284279316/ZcfbUjQNZ1KrsKkxq1xLiWCdWx-yZ361Fzd58wo8nZkDbL3XuoaDgJD99EhWb-EEOhkn"
